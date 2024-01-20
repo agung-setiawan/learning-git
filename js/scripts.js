17fa6a837b70e93b3ae0016ebf72f9cf83b79108
Agung Setiawan <agungsetiawan11@gmail.com>
2024-01-20 09:34:15 +0700

    Initial Scripts

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+/*!
+* Start Bootstrap - Blog Post v5.0.9 (https://startbootstrap.com/template/blog-post)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-blog-post/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+document.addEventListener("DOMContentLoaded", function(){
+  window.addEventListener('scroll', function() {
+      if (window.scrollY > 50) {
+        document.getElementById('navbar_top').classList.add('fixed-top');
+        // add padding top to show content behind navbar
+        navbar_height = document.querySelector('.navbar').offsetHeight;
+        document.body.style.paddingTop = navbar_height + 'px';
+      } else {
+        document.getElementById('navbar_top').classList.remove('fixed-top');
+         // remove padding top from body
+        document.body.style.paddingTop = '0';
+      } 
+  });
+}); 
